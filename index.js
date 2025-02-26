ce7ebb99d46fba8ccc206e8902d63708afa44b64
Radek <rad.szep@gmail.com>
2025-02-26 21:24:19 +0100

    Ukończony, to do http/s oraz delete buttons <li>

--- index.js
+++ index.js
@@ -1,19 +1,70 @@
 
 const inputBtn = document.getElementById('input-btn');
+const deleteBtn = document.getElementById('delete-btn');
+const tabBtn = document.getElementById('tab-btn');
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
 let myLeads = [];
+renderLeads();
+
 
 function saveLead(){
     if (inputEl.value !== ''){
         myLeads.push(inputEl.value);
         inputEl.value = '';
+        localStorage.setItem('myLeads', JSON.stringify(myLeads));
+        //local storage can only store strings
     }
-    ulEl.innerHTML = '';
-    for (let i = 0; i < myLeads.length; i++){
-        ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    }
+    renderLeads();
 }
 
-inputBtn.addEventListener('click', saveLead);
+
+function deleteLead(){
+    myLeads = [];
+    localStorage.clear();
+    renderLeads();
+}
+
+function saveTab(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem('myLeads', JSON.stringify(myLeads));
+        renderLeads();
+    });
+}
+
+
+function renderLeads(){
+    let listItems = "";
+    myLeads = JSON.parse(localStorage.getItem('myLeads'));
+    if (myLeads === null){
+        myLeads = [];
+    }
+
+    for (let i = 0; i < myLeads.length; i++){
+        //listItems += "<li>" + myLeads[i] + "</li>"   //or
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i];
+        // ulEl.append(li);
+
+
+        //or with link <a> tag
+        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        //for eg    <li><a href=' https://www.google.com '>www.google.com   </a></li>
+
+        //or ` ` allows to write in multiple lines and allows to use ${} to insert variables
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems;
+}
+
+inputBtn.addEventListener('click', saveLead)
+deleteBtn.addEventListener('click', deleteLead)
+tabBtn.addEventListener('click', saveTab)
 
